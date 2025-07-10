7b66f44cd11a72fd6862555470cf0516e781344c
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-07-10 12:43:47 +0100

    chapter 2: declare bean with Component.

---
+++ chapter2/src/main/java/com/znaji/springboot3recipes/Chapter2.java
@@ -0,0 +1,17 @@
+package com.znaji.springboot3recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class Chapter2
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Chapter2.class, args);
+    }
+}
